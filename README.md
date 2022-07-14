5a401da313cb9f699af73c2f52af84031975b10c
Minion <nessakristo@gmail.com>
2022-07-14 10:37:07 +0100

    new project

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # codecademyJS
 Just saving this here 
-project 1 : Convert temperature from kelvin to Celsius, then to Fahrenheit. 
+project 1 : Convert temperature from kelvin to Celsius, then to Fahrenheit.
+project 2 : Convert my age to dog age (lol) 
---
+++ dog age.js
@@ -0,0 +1,16 @@
+// create a variable myAge
+var myAge = 21
+// Create a variable named earlyYears
+var earlyYears = 2
+earlyYears *= 10.50
+let laterYears = myAge - 2;
+/*Multiply the laterYears variable by 4 to calculate the number of dog years accounted for by your later years.*/
+laterYears *= 4;
+console.log(earlyYears)
+console.log(laterYears)
+// add earlyYears and laterYears together
+myAgeInDogYears = earlyYears + laterYears
+/*The toLowerCase method returns a string with all lowercase letters.*/
+let myName = 'Vanessa'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+
